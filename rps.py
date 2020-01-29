9a4af793a7f4b50ef291c4d671745d17d72d76bf
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-01-29 00:34:09 -0500

    Reorganized the conditions for clarity purposes

--- rps.py
+++ rps.py
@@ -1,92 +1,107 @@
 # Rock, Paper, Scissors Game
 
+# import module
+import random
+
 # Ask Player 1 for move
 player_1 = input("Hey Player 1! Rock, Paper, Scissors?: ")
 
-# Print a bunch of lines to prevent cheating
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-print('No cheating!')
-
 # Play against the computer using randint
-import random
-computer = random.choice(['rock', 'paper', 'scissors'])
+num = random.randint(0, 2)
+if num == 0:
+	computer = 'rock'
+elif num == 1:
+	computer = 'paper'
+else:
+	computer = 'scissors'
+print(f"computer plays {computer}" )
 
 if player_1 == computer:
-	# If both players enter the same move
-	# Also works if both players do not enter anything
 	print("It's a tie!")
 elif player_1 == 'rock':
 	if computer == 'scissors':
 		print('player 1 wins')
+	elif computer == 'paper':
+		print('computer wins')
 elif player_1 == 'paper':
 	if computer == 'rock':
 		print('player 1 wins')
+	elif computer == 'scissors':
+		print('computer wins')
 elif player_1 == 'scissors':
 	if computer == 'paper':
 		print('player 1 wins')
+	elif computer == 'rock':
+		print('computer wins')
 else:
-	print("computer wins")
+	# Empty string or string does not match conditions
+	# Does not print out if player 1 enters the right input but player 2 does not
+	print("Something went wrong")
+
+# Print a bunch of lines to prevent cheating
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
+# print('No cheating!')
 
 # Ask Player 2 for move
-# computer = input("Hey Player 2! Rock, Paper, Scissors?: ")
+# player_2 = input("Hey Player 2! Rock, Paper, Scissors?: ")
 
 # Another round of refactoring?
 # else statement: Any situation where player 2 wins
 
 # Refactored code
-# if player_1 == computer:
+# if player_1 == player_2:
 # 	# If both players enter the same move
 # 	# Also works if both players do not enter anything
 # 	print("It's a tie!")
 # elif player_1 == 'rock':
-# 	if computer == 'paper':
+# 	if player_2 == 'scissors':
+# 		print('player 1 wins')
+# 	elif player_2 == 'paper':
 # 		print('player 2 wins')
-# 	elif computer == 'scissors':
-# 		print('player 1 wins')
 # elif player_1 == 'paper':
-# 	if computer == 'rock':
+# 	if player_2 == 'rock':
 # 		print('player 1 wins')
-# 	elif computer == 'scissors':
+# 	elif player_2 == 'scissors':
 # 		print('player 2 wins')
 # elif player_1 == 'scissors':
-# 	if computer == 'rock':
-# 		print('player 2 wins')
-# 	elif computer == 'paper':
+# 	if player_2 == 'paper':
 # 		print('player 1 wins')
+# 	elif player_2 == 'rock':
+# 		print('player 2 wins')
 # else:
 # 	# Empty string or string does not match conditions
 # 	# Does not print out if player 1 enters the right input but player 2 does not
 # 	print("Something went wrong")
 
 # # Conditional Logic
-# if player_1 == 'rock' and computer == 'paper':
+# if player_1 == 'rock' and player_2 == 'paper':
 # 	print('player 2 wins')
-# elif player_1 == 'rock' and computer == 'scissors':
+# elif player_1 == 'rock' and player_2 == 'scissors':
 # 	print('player 1 wins')
-# elif player_1 == 'paper' and computer == 'rock':
+# elif player_1 == 'paper' and player_2 == 'rock':
 # 	print('player 1 wins')
-# elif player_1 == 'paper' and computer == 'scissors':
+# elif player_1 == 'paper' and player_2 == 'scissors':
 # 	print('player 2 wins')
-# elif player_1 == 'scissors' and computer == 'rock':
+# elif player_1 == 'scissors' and player_2 == 'rock':
 # 	print('player 2 wins')
-# elif player_1 == 'scissors' and computer == 'paper':
+# elif player_1 == 'scissors' and player_2 == 'paper':
 # 	print('player 1 wins')
-# elif player_1 == computer:
+# elif player_1 == player_2:
 # 	print("It's a tie!")
 # else:
 # 	# Empty string or string does not match conditions
