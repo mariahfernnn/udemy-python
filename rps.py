0b09dd07779c2140aee233aee60da9ef63a707d3
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-01-28 23:22:10 -0500

    Added an else statement and combined all the elif statements where player 1 and player 2 inputs are the same

--- rps.py
+++ rps.py
@@ -30,17 +30,15 @@ if player_1 == 'rock' and player_2 == 'paper':
 	print('player 2 wins')
 elif player_1 == 'rock' and player_2 == 'scissors':
 	print('player 1 wins')
-elif player_1 == 'rock' and player_2 == 'rock':
-	print('Try again')
 elif player_1 == 'paper' and player_2 == 'rock':
 	print('player 1 wins')
 elif player_1 == 'paper' and player_2 == 'scissors':
 	print('player 2 wins')
-elif player_1 == 'paper' and player_2 == 'paper':
-	print('Try again')
 elif player_1 == 'scissors' and player_2 == 'rock':
 	print('player 2 wins')
 elif player_1 == 'scissors' and player_2 == 'paper':
 	print('player 1 wins')
-elif player_1 == 'scissors' and player_2 == 'scissors':
-	print('Try again')
+elif player_1 == player_2:
+	print("It's a tie!")
+else:
+	print("Something went wrong")
