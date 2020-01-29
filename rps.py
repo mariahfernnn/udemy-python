36cd004836f4ef9905ac880d24ab5286469a1159
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-01-28 23:32:21 -0500

    Testing each case - last elif and else statements don't work

--- rps.py
+++ rps.py
@@ -25,21 +25,43 @@ print('No cheating!')
 # Ask Player 2 for move
 player_2 = input("Hey Player 2! Rock, Paper, Scissors?: ")
 
-# Conditional Logic
-if player_1 == 'rock' and player_2 == 'paper':
-	print('player 2 wins')
-elif player_1 == 'rock' and player_2 == 'scissors':
-	print('player 1 wins')
-elif player_1 == 'paper' and player_2 == 'rock':
-	print('player 1 wins')
-elif player_1 == 'paper' and player_2 == 'scissors':
-	print('player 2 wins')
-elif player_1 == 'scissors' and player_2 == 'rock':
-	print('player 2 wins')
-elif player_1 == 'scissors' and player_2 == 'paper':
-	print('player 1 wins')
+# Refactored code
+if player_1 == 'rock':
+	if player_2 == 'paper':
+		print('player 2 wins')
+	elif player_2 == 'scissors':
+		print('player 1 wins')
+elif player_1 == 'paper':
+	if player_2 == 'rock':
+		print('player 1 wins')
+	elif player_2 == 'scissors':
+		print('player 2 wins')
+elif player_1 == 'scissors':
+	if player_2 == 'rock':
+		print('player 2 wins')
+	elif player_2 == 'paper':
+		print('player 1 wins')
 elif player_1 == player_2:
 	print("It's a tie!")
 else:
 	# Empty string or string does not match conditions
 	print("Something went wrong")
+
+# # Conditional Logic
+# if player_1 == 'rock' and player_2 == 'paper':
+# 	print('player 2 wins')
+# elif player_1 == 'rock' and player_2 == 'scissors':
+# 	print('player 1 wins')
+# elif player_1 == 'paper' and player_2 == 'rock':
+# 	print('player 1 wins')
+# elif player_1 == 'paper' and player_2 == 'scissors':
+# 	print('player 2 wins')
+# elif player_1 == 'scissors' and player_2 == 'rock':
+# 	print('player 2 wins')
+# elif player_1 == 'scissors' and player_2 == 'paper':
+# 	print('player 1 wins')
+# elif player_1 == player_2:
+# 	print("It's a tie!")
+# else:
+# 	# Empty string or string does not match conditions
+# 	print("Something went wrong")
