e2e585d66aee2469d224036d62b37bfbdeb8c11b
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-18 23:31:18 -0400

    Exercise: Emoji Art - using a while loop

--- emojiArt.py
+++ emojiArt.py
@@ -2,8 +2,10 @@
 
 # Print emojis using both a for loop and a while loop
 
-for x in range(1,11):
-	print("\U0001f600" * x)
-
-# while x != 11:
+# for x in range(1,11):
 # 	print("\U0001f600" * x)
+
+x = 1
+while x < 11:
+	print("\U0001f600" * x)
+	x += 1
