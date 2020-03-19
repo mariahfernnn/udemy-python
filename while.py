5fe7bf81f7c93beab0760e0dc947bbdc3fb066c5
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-18 23:01:20 -0400

    Converting for loops to while loops

--- while.py
+++ while.py
@@ -1,7 +1,16 @@
 # Introducing While Loops
 
-msg = input("What's the secret password?")
-while msg != "bananas":
-	print("WRONG!")
-	msg = input("What's the secret password?")
-print("CORRECT!")
+# msg = input("What's the secret password?")
+# while msg != "bananas":
+# 	print("WRONG!")
+# 	msg = input("What's the secret password?")
+# print("CORRECT!")
+
+# for num in range(1,11):
+# 	print(num)
+
+# convert it to a while loop
+num = 1
+while num < 11:
+	print(num)
+	num += 1
