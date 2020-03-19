bb2e04acb8f871737d462c900862c3a2092eeef9
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-18 22:38:50 -0400

    Exercise: Unlucky Numbers - first attempt without following along

---
+++ unluckyNumbers.py
@@ -0,0 +1,14 @@
+# EXERCISE: Unlucky Numbers
+
+# Loop through numbers 1-20 inclusive
+# for 4 and 13, print "x is unlucky"
+# for even numbers, print "x is even"
+# for odd numbers, print "x is odd"
+
+for x in range(1,21):
+	if x == 4 or x == 13:
+		print(f"{x} is UNLUCKY!")
+	elif (x % 2 == 0):
+		print(f"{x} is even")
+	else:
+		print(f"{x} is odd")
