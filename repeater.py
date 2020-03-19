77763da5d709879387e6ddde6734be2f52dc26fc
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-18 22:04:53 -0400

    Followed along solution - needed to convert str input to an int

--- repeater.py
+++ repeater.py
@@ -1,7 +1,9 @@
 # For loops in Python - More Exercise
 
 print("How many times did you walk Yoyo today?")
-walk = input()
+walks = input()
+# convert str to int
+walks = int(walks)
 
-for walk in "walks":
-	walk(print("I walked Yoyo today"))
+for walk in range(walks):
+	print("I walked Yoyo today")
