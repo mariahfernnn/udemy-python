2d9971b3be860079163b8f13af56976c0d5c49c7
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-24 16:40:30 -0400

    Exercise: Stop Copying Me - While Loops - first attempt

---
+++ copying.py
@@ -0,0 +1,9 @@
+# EXERCISE: Stop Copying Me - While Loop
+
+# Repeat user input until they say the magic word/phrase
+
+copy = input("Are we there yet? ")
+
+while copy != "stop copying me":
+	print(copy)
+	copy = input()
