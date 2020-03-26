4f0fc404247412252518461f07c771b807e58943
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-26 12:06:51 -0400

    Created guessing.py with starter code

---
+++ guessing.py
@@ -0,0 +1,12 @@
+# Section 11: Guessing Game 
+
+import random
+
+random_number = random.randint(1,10) # numbers 1 - 10 (inclusive)
+
+# handle user guesses
+#	if they guess CORRECT, tell them they won
+#		otherwise tell them if they are TOO HIGH or TOO LOW
+
+# BONUS - let player play again if they want
+
