b8805849a6f2025ea71ed509639a91987a31bd33
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-03-26 12:46:40 -0400

    Fixed issue - when user guesses correctly, user is now prompted that they are correct

--- guessing.py
+++ guessing.py
@@ -10,12 +10,15 @@ guess = int(guess)
 while guess != random_number:
 	if guess > random_number:
 		# print("TOO HIGH")
-		guess = input("TOO HIGH... Guess again")
+		guess = int(input("TOO HIGH... Guess again: "))
 	elif guess < random_number:
 		# print("TOO LOW")
-		guess = input("TOO LOW... Guess again")
-	else:
-		print("BINGO!")
+		guess = int(input("TOO LOW... Guess again: "))
+	# else:
+	# 	guess = int(input("BINGO!"))
+		# print("BINGO!")
+
+	print("BINGO! You are correct.")
 
 # handle user guesses
 #	if they guess CORRECT, tell them they won
