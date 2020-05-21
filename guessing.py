60a88755adedcd93d754edbc58c4a2335ee950dc
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-05-21 12:28:37 -0400

    Solution - follow-along

--- guessing.py
+++ guessing.py
@@ -18,20 +18,21 @@ while guess != random_number:
 	# 	guess = int(input("BINGO!"))
 		# print("BINGO!")
 
+print(random_number)
 print("BINGO! You are correct.")
 
-play = input("Do you want to keep playing? (y/n): ")
+# play = input("Do you want to keep playing? (y/n): ")
 
-while play != 'n':
-	while guess != random_number:
-		if guess > random_number:
-			# print("TOO HIGH")
-			guess = int(input("TOO HIGH... Guess again: "))
-		elif guess < random_number:
-			# print("TOO LOW")
-			guess = int(input("TOO LOW... Guess again: "))
-		else:
-			print("Thanks for playing. Bye!")
+# while play != 'n':
+# 	while guess != random_number:
+# 		if guess > random_number:
+# 			# print("TOO HIGH")
+# 			guess = int(input("TOO HIGH... Guess again: "))
+# 		elif guess < random_number:
+# 			# print("TOO LOW")
+# 			guess = int(input("TOO LOW... Guess again: "))
+# 		else:
+# 			print("Thanks for playing. Bye!")
 
 # handle user guesses
 #	if they guess CORRECT, tell them they won
